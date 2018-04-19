f3a8693db24fac32de9bbb3a92b42bd7e29b8174
Emily Jirles <emily.jirles@gmail.com>
2018-04-19 17:14:53 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,15 +1,15 @@
 // original my browswer didn't return numbers //
-//const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 //if going with strings and e.key //
-const code = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
+//const code = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"];
 
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
   function konamiAlert(e){
-    if (e.key === code[index]){
+    if (e.which === code[index]){
       index++;
-      console.log(e.key)
+      console.log(e.which)
       console.log(index)
       if (index === code.length){
          alert("Hurray!");
