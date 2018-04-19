e92b811b1f70604db891f2d26ba92d41cf571574
Emily Jirles <emily.jirles@gmail.com>
2018-04-19 16:41:02 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -6,7 +6,6 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 function init() {
   // Write your JavaScript code inside the init() function
   let index = 0;
-  
   function konamiAlert(e){
     var key = parseInt(e.which);
     if (key === code[index]){
