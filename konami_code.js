cbfabe166b70fe861947034598a16f7fabfd3970
Emily Jirles <emily.jirles@gmail.com>
2018-04-19 16:35:06 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,20 +1,23 @@
 // original my browswer didn't return numbers //
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 //if going with strings and e.key //
-//const codeStrings = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"] //
+//const codeStrings = ["ArrowUp", "ArrowUp", "ArrowDown", "ArrowDown", "ArrowLeft", "ArrowRight", "ArrowLeft", "ArrowRight", "b", "a"] 
 
 function init() {
   // Write your JavaScript code inside the init() function
   function konamiAlert(e){
-    for (let i = 0;i < 10;i++){
-      //if using codeStrings, use e.key//
-      if (e.which === parseInt(code[i])){
-        
-      } else {
-        return;
+    const key = parseInt(e.which);
+    let index = 0;
+    if (key === code[index]){
+      index++;
+      if (index === code.length){
+         alert("Nice Job :)");
+         index = 0
       }
+    } else {
+      index = 0
+      return;
     }
-    alert("Nice Job :)");
   }
   document.body.addEventListener('keydown', konamiAlert);
 }
